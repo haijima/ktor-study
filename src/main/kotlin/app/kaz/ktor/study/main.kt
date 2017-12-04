f29758e7ae2bc441d2a3a2f44e9caceb21e4cf21
haijima <k.haijima@gmail.com>
2017-12-05 01:08:56 +0900

    Make conf file

--- src/main/kotlin/app/kaz/ktor/study/main.kt
+++ src/main/kotlin/app/kaz/ktor/study/main.kt
@@ -1,20 +1,19 @@
 package app.kaz.ktor.study
 
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
-            }
+fun Application.main() {
+    install(CallLogging)
+    routing {
+        get("/") {
+            call.respondText("Hello, world!", ContentType.Text.Html)
         }
     }
-    server.start(wait = true)
 }
